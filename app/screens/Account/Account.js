fd8bed36f60d82533ef8b077e6f637205377ea4e
beatrizdominguez <bdominguez@influencity.com>
2021-09-05 17:06:08 +0200

    Loading coponent

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import { View, Text } from 'react-native'
 import * as firebase from 'firebase'
 import UserGuest from './UserGuest'
 import UserLogged from './UserLogged'
+import Loading from './../../components/Loading'
 
 export default function Account() {
     const [login, setLogin] = useState(null)
@@ -14,8 +14,7 @@ export default function Account() {
         })
     }, [])
 
-    if(login === null) return <Text>Cargando ...</Text>
-    
+    if(login === null) return <Loading isVisible={true} text='Cargando ...' />
 
     return login ? <UserLogged /> : <UserGuest />
 }

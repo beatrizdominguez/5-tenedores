4d53b02a71bf5256b5ea3e64782e97a6092a60c7
beatrizdominguez <bdominguez@influencity.com>
2021-09-05 16:28:03 +0200

    Add icons to menu

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { Icon } from 'react-native-elements'
 
 import RestaurantsStack from './RestaurantsStack'
 import FavoriteStack from './FavoriteStack'
@@ -13,7 +14,16 @@ const Tab = createBottomTabNavigator();
 export default function Navigation () {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName='account'
+        tabBarOptions={{
+          inactiveTintColor: '#646464',
+          activeTintColor:'#00a680'
+        }}
+        screenOptions={({route}) => ({
+          tabBarIcon : ({color}) => screenOptions(route, color)
+        })}
+      >
         <Tab.Screen
         name='restaurants'
         component={RestaurantsStack}
@@ -44,3 +54,34 @@ export default function Navigation () {
     </NavigationContainer>
   )
 }
+
+const screenOptions = (route, color) => {
+  let iconName 
+  switch(route.name){
+    case 'restaurants':
+      iconName = 'compass-outline'
+      break;
+    case 'favorites':
+      iconName = 'heart-outline'
+      break;
+    case 'topRestaurants':
+      iconName = 'star-outline'
+      break;
+    case 'search':
+      iconName = 'magnify'
+      break;
+    case 'account':
+      iconName = 'home-outline'
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon 
+      type='material-community' 
+      name={iconName}
+      size={22}
+      color={color}
+    />
+    ) 
+  }

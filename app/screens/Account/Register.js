32e755582ab6bac4e40f74916ad3d5b6fe6a567f
beatrizdominguez <bdominguez@influencity.com>
2021-09-05 18:12:55 +0200

    Keyboard on input focus

---
+++ app/screens/Account/Register.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
+import { Divider } from 'react-native-elements'
+import RegisterForm from './../../components/Account/RegisterForm'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+export default function Register() {
+
+    return (
+        <KeyboardAwareScrollView>
+          <Image
+                source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+                resizeMode="contain"
+                style={styles.logo}
+            />    
+            <View style={styles.viewForm}>
+                <RegisterForm />
+            </View>
+        </KeyboardAwareScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    logo: {
+      width: "100%",
+      height: 150,
+      marginTop: 20,
+    },
+    viewForm: {
+      marginRight: 40,
+      marginLeft: 40,
+    },
+  });

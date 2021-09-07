a8049b4d65f53db46416bb5d936797a3c945f703
beatrizdominguez <bdominguez@influencity.com>
2021-09-07 22:43:08 +0200

    Form inputs

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -4,80 +4,118 @@ import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
 
 
 export default function AddRestaurantForm(props) {
-  const { toastRef, setIsLoading, navigation } = props;
+    const { toastRef, setIsLoading, navigation } = props;
 
-  return (
-      <View>
-          <Text>add form</Text>
-      </View>
-  )
+
+    return (
+        <ScrollView style={styles.scrollView}>
+            <FormAdd />
+        </ScrollView>
+    )
+}
+
+function FormAdd(props) {
+    const {
+        setRestaurantName,
+        setRestaurantAddress,
+        setRestaurantDescription,
+        setIsVisibleMap,
+        locationRestaurant,
+    } = props;
+
+    return (
+        <View style={styles.viewForm}>
+            <Input
+                placeholder="Nombre del restaurante"
+                containerStyle={styles.input}
+                // onChange={(e) => setRestaurantName(e.nativeEvent.text)}
+            />
+            <Input
+                placeholder="Dirección"
+                containerStyle={styles.input}
+                // onChange={(e) => setRestaurantAddress(e.nativeEvent.text)}
+                // rightIcon={{
+                //     type: "material-community",
+                //     name: "google-maps",
+                //     color: locationRestaurant ? "#00a680" : "#c2c2c2",
+                //     onPress: () => setIsVisibleMap(true),
+                // }}
+            />
+            <Input
+                placeholder="Descripcion del restaurante"
+                multiline={true}
+                inputContainerStyle={styles.textArea}
+                // onChange={(e) => setRestaurantDescription(e.nativeEvent.text)}
+            />
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
-  scrollView: {
-    height: "100%",
-  },
-  viewForm: {
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  input: {
-    marginBottom: 10,
-  },
-  textArea: {
-    height: 100,
-    width: "100%",
-    padding: 0,
-    margin: 0,
-  },
-  btnAddRestaurant: {
-    backgroundColor: "#00a680",
-    margin: 20,
-  },
-  viewImages: {
-    flexDirection: "row",
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 30,
-  },
-  containerIcon: {
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 10,
-    height: 70,
-    width: 70,
-    backgroundColor: "#e3e3e3",
-  },
-  miniatureStyle: {
-    width: 70,
-    height: 70,
-    marginRight: 10,
-  },
-  viewPhoto: {
-    alignItems: "center",
-    height: 200,
-    marginBottom: 20,
-  },
-  mapStyle: {
-    width: "100%",
-    height: 550,
-  },
-  viewMapBtn: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 10,
-  },
-  viewMapBtnContainerCancel: {
-    paddingLeft: 5,
-  },
-  viewMapBtnCancel: {
-    backgroundColor: "#a60d0d",
-  },
-  viewMapBtnContainerSave: {
-    paddingRight: 5,
-  },
-  viewMapBtnSave: {
-    backgroundColor: "#00a680",
-  },
+    scrollView: {
+        height: "100%",
+    },
+    viewForm: {
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    input: {
+        marginBottom: 10,
+    },
+    textArea: {
+        height: 100,
+        width: "100%",
+        padding: 0,
+        margin: 0,
+    },
+    btnAddRestaurant: {
+        backgroundColor: "#00a680",
+        margin: 20,
+    },
+    viewImages: {
+        flexDirection: "row",
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 30,
+    },
+    containerIcon: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginRight: 10,
+        height: 70,
+        width: 70,
+        backgroundColor: "#e3e3e3",
+    },
+    miniatureStyle: {
+        width: 70,
+        height: 70,
+        marginRight: 10,
+    },
+    viewPhoto: {
+        alignItems: "center",
+        height: 200,
+        marginBottom: 20,
+    },
+    mapStyle: {
+        width: "100%",
+        height: 550,
+    },
+    viewMapBtn: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 10,
+    },
+    viewMapBtnContainerCancel: {
+        paddingLeft: 5,
+    },
+    viewMapBtnCancel: {
+        backgroundColor: "#a60d0d",
+    },
+    viewMapBtnContainerSave: {
+        paddingRight: 5,
+    },
+    viewMapBtnSave: {
+        backgroundColor: "#00a680",
+    },
 });

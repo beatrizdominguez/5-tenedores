9b4fd6da8597af66654fb3c74ba88a9170724c1e
beatrizdominguez <bdominguez@influencity.com>
2021-09-07 22:36:56 +0200

    Add form

---
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ScrollView, Alert, Dimensions, Text } from "react-native";
+import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+
+
+export default function AddRestaurantForm(props) {
+  const { toastRef, setIsLoading, navigation } = props;
+
+  return (
+      <View>
+          <Text>add form</Text>
+      </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  scrollView: {
+    height: "100%",
+  },
+  viewForm: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  btnAddRestaurant: {
+    backgroundColor: "#00a680",
+    margin: 20,
+  },
+  viewImages: {
+    flexDirection: "row",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 30,
+  },
+  containerIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+    height: 70,
+    width: 70,
+    backgroundColor: "#e3e3e3",
+  },
+  miniatureStyle: {
+    width: 70,
+    height: 70,
+    marginRight: 10,
+  },
+  viewPhoto: {
+    alignItems: "center",
+    height: 200,
+    marginBottom: 20,
+  },
+  mapStyle: {
+    width: "100%",
+    height: 550,
+  },
+  viewMapBtn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  viewMapBtnContainerCancel: {
+    paddingLeft: 5,
+  },
+  viewMapBtnCancel: {
+    backgroundColor: "#a60d0d",
+  },
+  viewMapBtnContainerSave: {
+    paddingRight: 5,
+  },
+  viewMapBtnSave: {
+    backgroundColor: "#00a680",
+  },
+});
--- app/screens/Restaurants/AddRestaurant.js
+++ app/screens/Restaurants/AddRestaurant.js
@@ -1,24 +1,26 @@
 import React, { useState, useRef } from "react";
 import { View, Text } from "react-native";
 import Toast from "react-native-easy-toast";
+import { useNavigation } from '@react-navigation/native'
 import Loading from "../../components/Loading";
-// import AddRestaurantForm from "../../components/Restaurants/AddRestaurantForm";
+import AddRestaurantForm from "../../components/Restaurants/AddRestaurantForm";
 
 export default function AddRestaurant(props) {
 //   const { navigation } = props
   const [isLoading, setIsLoading] = useState(false)
   const toastRef = useRef()
+  const navigation = useNavigation()
 
   return (
     <View>
         <Text>add restaurant</Text>
-      {/* <AddRestaurantForm
+      <AddRestaurantForm
         toastRef={toastRef}
         setIsLoading={setIsLoading}
-        navigation={navigation}
-      /> */}
-      {/* <Toast ref={toastRef} position="center" opacity={0.9} /> */}
-      {/* <Loading isVisible={isLoading} text="Creando restaurante" /> */}
+        // navigation={navigation}
+      />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+      <Loading isVisible={isLoading} text="Creando restaurante" />
     </View>
   );
 }

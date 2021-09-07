6ff7b9939d7d1d54ba4b071d69fc729185fa68ea
beatrizdominguez <bdominguez@influencity.com>
2021-09-07 22:23:59 +0200

    Visible only when logged

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from "react-native";
 import { Icon } from "react-native-elements";
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
 
 export default function Restaurants() {
+    const [user, setUser] = useState(null)
+
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((userInfo) => {
+            setUser(userInfo)
+        })
+    }, [])
+
     return (
         <View style={styles.viewBody}>
             <Text>Restaurantes aqui</Text>
-            <Icon
-                reverse
-                type="material-community"
-                name="plus"
-                color="#00a680"
-                containerStyle={styles.btnContainer}
+            {user && (
+                <Icon
+                    reverse
+                    type="material-community"
+                    name="plus"
+                    color="#00a680"
+                    containerStyle={styles.btnContainer}
                 // onPress={() => navigation.navigate("add-restaurant")}
-            />
+                />
+            )}
         </View>
     )
 }

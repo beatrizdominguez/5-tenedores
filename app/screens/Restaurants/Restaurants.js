ba4051c7bf0e6d3f110ec89e6ec62795507d2e03
beatrizdominguez <bdominguez@influencity.com>
2021-09-07 22:20:03 +0200

    Boton flotante

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-elements";
 
 export default function Restaurants() {
     return (
-        <View>
+        <View style={styles.viewBody}>
             <Text>Restaurantes aqui</Text>
+            <Icon
+                reverse
+                type="material-community"
+                name="plus"
+                color="#00a680"
+                containerStyle={styles.btnContainer}
+                // onPress={() => navigation.navigate("add-restaurant")}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    btnContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+        shadowColor: "black",
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.5,
+    },
+});

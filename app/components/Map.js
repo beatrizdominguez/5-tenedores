672198559b83703ab11f3376942e28d4028823bc
beatrizdominguez <bdominguez@influencity.com>
2021-09-10 18:16:39 +0200

    Open google maps

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,16 +1,17 @@
 import React from "react"
 import MapView from "react-native-maps"
-// import openMap from "react-native-open-maps"
+import openMap from "react-native-open-maps"
 
 export default function Map(props) {
   const { location, name, height } = props;
 
   const openAppMap = () => {
-    // openMap({
-    //   latitude: location.latitude,
-    //   longitude: location.longitude,
-    //   zoom: 19,
-    // });
+    openMap({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      zoom: 19,
+      query: name
+    });
   };
 
   return (

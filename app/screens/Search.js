f4cfb0efafb0256d1947036b7635a2310bbed88e
beatrizdominguez <bdominguez@influencity.com>
2021-09-11 10:18:17 +0200

    Search bar

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, FlatList, Image } from "react-native";
+import { SearchBar, ListItem, Icon } from "react-native-elements";
+import { useNavigation, useRoute } from "@react-navigation/native"
 
 export default function Search() {
+    const navigation = useNavigation()
+    const [search, setSearch] = useState("");
+
     return (
         <View>
-            <Text>Search page</Text>
+            <SearchBar
+                placeholder="Busca tu restaurante..."
+                onChangeText={(e) => setSearch(e)}
+                value={search}
+                containerStyle={styles.searchBar}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    searchBar: {
+        marginBottom: 20,
+    },
+});
